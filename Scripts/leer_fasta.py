265798ab1a475f2504510325a97fcc1047b5e1ec
Oscar Gabriel Flores Aguñagaip <oscar.flores300107@gmail.com>
2026-06-15 16:05:27 -0600

    The ability to read all types of DNA sequences was added and count the nucleotides ('AGTC')

--- Scripts/leer_fasta.py
+++ Scripts/leer_fasta.py
@@ -4,6 +4,14 @@ from pathlib import Path
 ruta_fasta = Path(__file__).parent.parent / "datos" / "ejemplo.fasta"
 
 for record in SeqIO.parse(ruta_fasta, "fasta"):
+    Secuencia = record.seq.upper()
     print("ID:", record.id)
-    print("Longitud:", len(record.seq))
-    print("Secuencia:", record.seq)
+    print("Longitud:", len(Secuencia))
+    if len(Secuencia) > 50:
+        print("Secuencia (primeros 50 caracteres):", Secuencia[:50])
+  
+    for letra in "ACGT":
+        print(f"Cantidad de {letra}: {Secuencia.count(letra)}")
+    porcent_gc = 100 * (Secuencia.count("G") + Secuencia.count("C")) / len(Secuencia)
+    print(f"Porcentaje GC: {porcent_gc:.2f}%")
+
